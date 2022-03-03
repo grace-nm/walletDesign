5a64d4b4880180cd4dd3fa787c1f79bc54e3057e
grace-nm <grace.mcloughlin@ezyvet.com>
2022-03-03 21:53:24 +1300

    further building of files

--- App.tsx
+++ App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import Welcome from './screens/Welcome';
+import Welcome from "./screens/Welcome";
+//custom font
+import AppLoading from "expo-app-loading";
+import { useFonts } from "expo-font";
 
 export default function App() {
-  return (
-    <Welcome />
-  );
-}
+  let [fontsLoaded] = useFonts({
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "Lato-Regular": require("./assets/fonts/Lato-Bold.ttf"),
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (<Welcome />);
+}
